f078d1170aa5337493da00a393e88a335f95cbfd
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2022-01-11 19:30:36 +0500

    Demo Filter

---
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -0,0 +1,36 @@
+package ru.job4j.dream.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String uri = req.getRequestURI();
+        if (uri.endsWith("auth.do")) {
+            chain.doFilter(req, resp);
+            return;
+        }
+        if (req.getSession().getAttribute("user") == null) {
+           resp.sendRedirect(req.getContextPath() + "/login.jsp");
+           return;
+        }
+        chain.doFilter(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
